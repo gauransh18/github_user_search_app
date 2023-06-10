e97b55d406c6f257536e7db7d6f8bdc2d7db75f6
gauransh18 <66783709+gauransh18@users.noreply.github.com>
2023-06-11 03:16:31 +0530

    ui updated

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color backgroundColor = const Color(0xFF0e1116);
+Color dotColor = const Color.fromARGB(255, 55, 66, 79);
--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:grid_paper/grid_paper.dart';
+import 'constants.dart';
 
 class Home extends StatefulWidget {
-  const Home({super.key});
+  const Home({Key? key}) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
@@ -12,44 +15,113 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      backgroundColor: Color(0xff0e1116),
-      body: SizedBox(
-        height: double.infinity,
-        width: double.infinity,
-        child: GridPaper(
-          color: Color.fromARGB(255, 55, 66, 79),
-          interval: 100,
-          divisions: 1,
-          subdivisions: 2,
-          child: Column(children: [
-            Container(
-              height: 40,
-              width: MediaQuery.of(context).size.width - 20,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.white,
-                  width: 2,
-                ),
-                borderRadius: BorderRadius.circular(12),
+      backgroundColor: backgroundColor,
+      body: Stack(
+        children: [
+          SizedBox(
+            height: height,
+            width: width,
+            child: DotMatrixPaper(
+              gridUnitSize: 25,
+              originAlignment: Alignment.center,
+              background: backgroundColor, // const Color(0xFF444444),
+              style: const DotMatrixStyle.standard().copyWith(
+                dotColor: dotColor,
+                divider: DotMatrixDivider.biggerDot,
+                dividerColor: dotColor,
               ),
-              child: Row(
-                children: [],
+            ),
+          ),
+          Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // SizedBox(
+                  //   height: height * 0.3,
+                  // ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundImage: AssetImage(
+                        'assets/main.png',
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Center(
+                    child: Container(
+                      height: 60,
+                      width: width - 40,
+                      decoration: BoxDecoration(
+                        color: backgroundColor,
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              style: GoogleFonts.abel(
+                                color: Colors.white,
+                                fontSize: 22,
+                              ),
+                              cursorColor: Colors.white,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'username',
+                                hintStyle: GoogleFonts.abel(
+                                  color: Colors.white.withOpacity(0.6),
+                                  fontSize: 22,
+                                ),
+                                contentPadding: EdgeInsets.all(12),
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              // Perform search here
+                            },
+                            icon: Icon(
+                              Icons.search,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
               ),
-            )
-          ]),
-        ),
+            ),
+          ),
+        ],
       ),
-      bottomSheet: Container(
-        decoration: BoxDecoration(
-          color: Color.fromARGB(255, 55, 66, 79),
-        ),
-        height: MediaQuery.of(context).size.height * 0.1,
-        width: MediaQuery.of(context).size.width,
-        child: Row(
-          children: [],
-        ),
-      ),
+
+      //BOTTOM SHEET
+
+      // bottomSheet: Container(
+      //   decoration: BoxDecoration(
+      //     color: Color.fromARGB(255, 55, 66, 79),
+
+      //   ),
+      //   height: MediaQuery.of(context).size.height * 0.1,
+      //   width: MediaQuery.of(context).size.width,
+      //   child: Row(
+      //     children: [],
+      //   ),
+      // ),
     );
   }
 }
