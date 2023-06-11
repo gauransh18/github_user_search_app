415564cd916c6a0539706d497a633240bc539251
gauransh18 <66783709+gauransh18@users.noreply.github.com>
2023-06-11 21:46:12 +0530

    search done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'search.dart';
 import 'home.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Home(),
+      home: const Search(),
     );
   }
 }
